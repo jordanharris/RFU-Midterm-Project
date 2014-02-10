fdf698d658392fa8edcf50c895c7d0205f55521e
jordanharris <jordanburrows@gmail.com>
2014-02-10 10:25:02 -0700

    First commit of Midterm Project

---
+++ js/gmapsAPI.js
@@ -0,0 +1,207 @@
+$(function(){
+	function initialize() {
+        var mapOptions = {
+        	center: new google.maps.LatLng(40.0076, 254.744),
+        	zoom: 12,
+        	panControl: false,
+        	maxZoom: 12,
+        	minZoom: 12,
+        	streetViewControl: false,
+        	mapTypeControl: false,
+        	zoomControl: false
+        };
+        //creates the google map api
+        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+        //create ovrelapping markers plug-in that separates markers that are on the same latLng
+        var oms = new OverlappingMarkerSpiderfier(map);
+        var iw = new google.maps.InfoWindow();
+		oms.addListener('click', function(marker, event) {
+			iw.setContent(marker.desc);
+			iw.open(map, marker);
+		});
+		oms.addListener('spiderfy', function(markers) {
+			iw.close();
+		});
+
+		//creates info window for each marker
+		// var infowindow = new google.maps.InfoWindow({
+  //     		content: "hey all!"
+  // 		});
+
+        //Create static markers on the map when map loads
+        //creates variable of marker images to be used when the marker is placed
+        var markerImages = [
+        	{image: $("#jencon").attr("src")},
+        	{image: $("#limacon").attr("src")},
+        	{image: $("#rosiecon").attr("src")},
+        	{image: $("#arleniscon").attr("src")},
+        	{image: $("#jarahcon").attr("src")}
+        ];
+
+        //list of cities and their LatLng positions that giranaut has launched in
+  		var markerLatLng = [
+        {locate: new google.maps.LatLng(40.71, 285.994)},
+        {locate: new google.maps.LatLng(40.0076, 254.744)},
+        {locate: new google.maps.LatLng(41.8519, 272.344)},
+        {locate: new google.maps.LatLng(36.09, 244.794)}
+        ];
+
+        //creates the marker that is to be placed
+        function markerCreate(arrImages,arrLatLng ){
+        	for(var i in arrLatLng){
+        		for(var j = 0; j<arrImages.length ; j++){
+			        var marker = new google.maps.Marker({
+			            position: arrLatLng[i].locate,
+			            map: map,
+			            icon: arrImages[j].image,
+			            animation: google.maps.Animation.DROP,
+			            draggable: true
+			        });
+			        oms.addMarker(marker);
+		    	}
+		    }
+	    }
+
+        //click handler to zoom in on city after button has been clicked
+         $(".giranaut-cities li").on("click", function(){
+        	if($(this).text()==="Boulder"){
+        		map.setCenter(markerLatLng[1].locate);
+				map.setZoom(12);
+			}
+			else if($(this).text()==="NYC"){
+				map.setCenter(markerLatLng[0].locate);
+				map.setZoom(12);
+			}
+			else if($(this).text()==="Chicago"){
+				map.setCenter(markerLatLng[2].locate);
+				map.setZoom(12);
+			}
+			else{
+				map.setCenter(markerLatLng[3].locate);
+				map.setZoom(12);
+			}
+		})
+
+  //       google.maps.event.addListener(marker, 'click', function() {
+		//     infowindow.open(map,marker);
+		// });
+
+	    //calls the create marker function and places markers on the map
+	    markerCreate(markerImages, markerLatLng);
+
+
+
+
+        //google maps styling of maps object:	
+	    var stylers = [
+	    	{
+	   		"featureType": "transit",
+	    	"stylers": [{ 
+	    		"visibility": "off" 
+	    	}]
+	  		},
+	  		{
+	    	"featureType": "road",
+	    	"stylers": [{
+	    		"visibility": "simplified" 
+	    	}]
+	  		},
+	  		{
+	    	"featureType": "landscape",
+	    	"elementType": "labels",
+	    	"stylers": [{
+	    		"visibility": "off" 
+	    	}]
+	  		},
+	  		{
+	  		"featureType": "landscape.natural.terrain",
+	  		"elementType": "geometry",
+	  		"stylers": [{
+	  			"visibility": "on" 
+	  		}]
+	  		},
+	  		{
+	    	"featureType": "poi",
+	    	"stylers": [{
+	    		"visibility": "off" 
+	    	}]
+	  		},
+	  		{
+	    	"featureType": "water",
+	    	"stylers": [{
+	    		"visibility": "simplified" 
+	    	}]
+			},
+			{
+	    	"featureType": "road",
+	    	"elementType": "labels",
+	    	"stylers": [{
+	    		"visibility": "off" 
+	    	}]
+	  		},
+	  		{
+	    	"featureType": "administrative.land_parcel",
+	    	"stylers": [{
+	    		"visibility": "off" 
+	    	}]
+	  		},
+	  		{
+	    	"featureType": "administrative.neighborhood",
+	    	"stylers": [{
+	    		"visibility": "off" 
+	    	}]
+	  		},
+	  		{
+	    	"featureType": "administrative.province",
+	    	"stylers": [{
+	    		"visibility": "off" 
+	    	}]
+	  		},
+	  		{
+	    	"featureType": "administrative",
+	    	"elementType": "geometry",
+	    	"stylers": [{
+	    		"visibility": "off" 
+	    	}]
+	  		},
+	  		{
+	    	"featureType": "water",
+	    	"elementType": "geometry",
+	    	"stylers": [{
+	    		"color": "#99f4ff" 
+	    	}]
+	  		},
+	  		{
+	    	"featureType": "landscape",
+	    	"stylers": [{
+	    		"color": "#31a745" 
+	    	}]
+	  		},
+	  		{
+	    	"elementType": "labels",
+	    	"stylers": [{
+	    		"weight": 0.5 
+	    	},
+	      	{
+	      		"color": "#0000ff" 
+	      	}]
+	  		}
+		]
+		//calls setOptions and makes map styles to styles listed above:
+		map.setOptions({styles: stylers});
+
+
+
+
+	
+
+
+
+
+
+
+
+	}
+    google.maps.event.addDomListener(window, 'load', initialize);
+})
---
+++ js/midtermproject.js
@@ -0,0 +1,127 @@
+$(function(){
+	//cycle through background Images
+	var imageCycle = ["imageCycle1", "imageCycle2"/*, "imageCycle3"*/];
+	var randomNumber = Math.floor(Math.random()*imageCycle.length);
+	$(".landingPageSection").addClass(imageCycle[randomNumber]);
+
+
+	// $(".signInButton").on("click", function(){
+	// 	$(this).toggleClass("toggleSignInBorderRadius");
+	// 	$(".signInForm").toggle();
+	// })
+	
+	//scroll to navbar name tage on page
+	$(document).on("click", ".navbar-brand", function(){
+		$('body').animatescroll();
+	})
+	$(document).on("click", "#tourists", function(){
+		$('#touristInfo').animatescroll();
+	})
+	$(document).on("click", "#tourGuides", function(){
+		$('#tourGuideInfo').animatescroll();
+	})
+	$(document).on("click", "#contactUs", function(){
+		$('#contactInfo').animatescroll();
+	})
+	//scroll to #touristInfo on Explore GiraNaut click
+	$(document).on("click", "#explore", function(){
+		$('#touristInfo').animatescroll();
+	})
+
+	//adds class active to menu bar to highlight the location you're at on the page
+	// $(".navGuide").mouseenter(function(){
+	// 	// $(".navGuide").removeClass("active");
+	// 	$(this).addClass("active");},
+	// 	function(){
+	// 	$(this).removeClass("active");
+	// 	}
+	// 	);
+
+	$(window).on("scroll", function(){
+		var windowLocation = $(this).scrollTop();
+		var navbarHeight = $(".navbar-inverse").height();
+		var sectionTourists = $("#touristInfo").offset().top;
+		var sectionTourGuides = $("#tourGuideInfo").offset().top;
+		var sectionContactUs = $("#contactInfo").offset().top;
+		if(windowLocation < (sectionTourists - navbarHeight)){
+			$(".navGuide").removeClass("active");
+		}
+		else if(windowLocation >= (sectionTourists - navbarHeight) && windowLocation < (sectionTourGuides - navbarHeight)){
+			$(".navGuide").removeClass("active");
+			$("#tourists").addClass("active");
+		}
+		else if(windowLocation >= (sectionTourGuides - navbarHeight) && windowLocation < (sectionContactUs - navbarHeight)){
+			$(".navGuide").removeClass("active");
+			$("#tourGuides").addClass("active");
+		}
+		else{
+			$(".navGuide").removeClass("active");
+			$("#contactUs").addClass("active");
+		}
+	})
+
+
+	//regex checks for signup validation
+	$("#submitSignIn").on("click", function(){	
+		var email = $("#emailSignIn").val();
+		var password = $("#passwordSignIn").val();
+		var mailformat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z\s]{2,4}$/;
+		var passwordformat = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])\w{6,}$/;
+		if (mailformat.test(email) === false) 
+		{
+			alert("Not a valid e-mail address");
+			return false;	
+		}
+		if (passwordformat.test(password) === false)
+		{
+			alert("Password must contain at least one Uppercase letter, one Lowercase letter, and one Number");
+			return false;
+		}
+	});	
+
+	//hover over social media for image effect
+	$(".socialMediaImageTwitter").hover(function(){
+		$(this).attr("src", "css/img/twitterhover.png");
+	}, function(){
+		$(this).attr("src", "css/img/twitter.png");
+	});
+
+	$(".socialMediaImageFacebook").hover(function(){
+		$(this).attr("src", "css/img/facebookhover.png");
+	}, function(){
+		$(this).attr("src", "css/img/facebook.png");
+	});
+
+	$(".socialMediaImagePinterest").hover(function(){
+		$(this).attr("src", "css/img/pinteresthover.png");
+	}, function(){
+		$(this).attr("src", "css/img/pinterest.png");
+	});
+
+	$(".socialMediaImageGooglePlus").hover(function(){
+		$(this).attr("src", "css/img/googleplushover.png");
+	}, function(){
+		$(this).attr("src", "css/img/googleplus.png");
+	});
+
+	$(".socialMediaImageTwitter").on("click", function(){
+		$(this).attr("src", "css/img/twitteractive.png");
+	});
+
+	$(".socialMediaImageFacebook").on("click", function(){
+		$(this).attr("src", "css/img/facebookactive.png");
+	});
+
+	$(".socialMediaImagePinterest").on("click", function(){
+		$(this).attr("src", "css/img/pinterestactive.png");
+	});
+
+	$(".socialMediaImageGooglePlus").on("click", function(){
+		$(this).attr("src", "css/img/googleplusactive.png");
+	});
+
+
+
+
+
+})
